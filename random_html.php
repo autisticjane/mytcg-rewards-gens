1440b827334ca787c5228bfa0889d198e1427de6
Jane Lively <4278716+izzylively@users.noreply.github.com>
2022-09-18 10:47:38 -0500

    Create random_html.php

---
+++ random_html.php
@@ -0,0 +1,56 @@
+<?php include("mytcg/settings.php");
+include("$header");
+?>
+<h1>HTML Generator</h1>
+<div class="random-generator">
+	<form method="post" action="random_html.php?pickup">
+		<label for="cards">Your cards, separated by commas:</label>
+		<textarea name="cards" id="cards" style="width: 300px; height: 100px" placeholder="Your cards separated by commas"></textarea><br />
+		<input type="submit" name="submit" value=" Go! " />
+	</form>
+</div>
+		<?php
+
+if($_SERVER['QUERY_STRING']=="pickup") {
+	if (!isset($_POST['submit']) || $_SERVER['REQUEST_METHOD'] != "POST") {
+		exit("<p>You did not press the submit button; this page should not be accessed directly.</p>");
+	}
+	else {
+		$exploits = "/(content-type|bcc:|cc:|document.cookie|onclick|onload|javascript|alert)/i";
+		$profanity = "/(beastial|bestial|blowjob|clit|cunilingus|cunillingus|cunnilingus|cunt|ejaculate|fag|felatio|fellatio|fuck|fuk|fuks|gangbang|gangbanged|gangbangs|hotsex|jism|jiz|kock|kondum|kum|kunilingus|orgasim|orgasims|orgasm|orgasms|phonesex|phuk|phuq|porn|pussies|pussy|spunk|xxx)/i";
+		$spamwords = "/(viagra|phentermine|tramadol|adipex|advai|alprazolam|ambien|ambian|amoxicillin|antivert|backgammon|holdem|poker|carisoprodol|ciara|ciprofloxacin|debt|dating|porn)/i";
+		$bots = "/(Indy|Blaiz|Java|libwww-perl|Python|OutfoxBot|User-Agent|PycURL|AlphaServer)/i";
+		
+		if (preg_match($bots, $_SERVER['HTTP_USER_AGENT'])) {
+			exit("<h1>Error</h1>\nKnown spam bots are not allowed.<br /><br />");
+			}
+			foreach ($_POST as $key => $value) {
+				$value = trim($value);
+				if (empty($value)) {
+					exit("<h1>Error</h1>\nEmpty fields are not allowed. Please go back and fill in the form properly.<br /><br />");
+				}
+				elseif (preg_match($exploits, $value)) {
+					exit("<h1>Error</h1>\nExploits/malicious scripting attributes aren't allowed.<br /><br />");
+				}
+				elseif (preg_match($profanity, $value) || preg_match($spamwords, $value)) {
+					exit("<h1>Error</h1>\nThat kind of language is not allowed through our form.<br /><br />");
+				}
+				$_POST[$key] = stripslashes(strip_tags($value));
+			}
+				?>
+<div class="random-results">
+	<textarea onclick="this.focus();this.select()" style="width: 75%;">
+		<?php
+			echo "<img src=\"/cards/";
+			$find[] = ', ';
+			$replace[] = '.png" alt="" /> <img src="/cards/';
+			$text = str_replace($find, $replace, $_POST['cards']);
+			print_r($text);
+		?>.png" alt="" />
+	</textarea>
+</div>
+				<?php
+			}
+}
+include("$footer");
+?>
